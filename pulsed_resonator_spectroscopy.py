b2f3eae1b85f230e3fb07b9428f723fe2ad47800
Christian Križan <christian.krizan@gmail.com>
2022-02-15 20:22:03 +0100

    Enabled |1⟩ state spectroscopy.
    The pulsed resonator spectroscopy can now also be used to find the resonator position for the |1⟩-state. The "excited state readout position."

--- pulsed_resonator_spectroscopy.py
+++ pulsed_resonator_spectroscopy.py
@@ -61,7 +61,6 @@ def find_f_ro01_sweep_coupler(
         "z_scaler": 1.0,
         "z_unit":   'default',
         }
-    
     ):
     ''' Find the optimal readout frequency for reading out qubits in state |0>,
         as a function of a swept pairwise coupler bias.
@@ -70,8 +69,6 @@ def find_f_ro01_sweep_coupler(
     ## Input sanitisation
     
     # Acquire legal values regarding the coupler port settings.
-    first_msg  = ''
-    second_msg = ''
     if num_biases < 1:
         num_biases = 1
         print("Note: num_biases was less than 1, and was thus set to 1.")
@@ -305,6 +302,7 @@ def find_f_ro01_sweep_coupler(
             'sampling_duration', "s",
             'readout_sampling_delay', "s",
             'repetition_delay', "s", 
+            
             #'coupler_dc_port', "",
             'added_delay_for_bias_tee', "s",
             'num_freqs', "",
@@ -739,6 +737,8 @@ def find_f_ro12_sweep_coupler(
     sampling_duration,
     readout_sampling_delay,
     repetition_delay,
+    integration_window_start,
+    integration_window_stop,
     
     control_port,
     control_amp_01,
@@ -754,11 +754,43 @@ def find_f_ro12_sweep_coupler(
     num_biases,
     coupler_bias_min = -1.0,
     coupler_bias_max = +1.0,
+    
     save_complex_data = False,
+    use_log_browser_database = True,
+    axes =  {
+        "x_name":   'default',
+        "x_scaler": 1.0,
+        "x_unit":   'default',
+        "y_name":   'default',
+        "y_scaler": 1.0,
+        "y_unit":   'default',
+        "z_name":   'default',
+        "z_scaler": 1.0,
+        "z_unit":   'default',
+        }
     ):
     ''' Find the optimal readout frequency for reading out qubits in state |1>,
         as a function of a swept pairwise coupler bias.
     '''
+    
+    ## Input sanitisation
+    
+    # Acquire legal values regarding the coupler port settings.
+    if num_biases < 1:
+        num_biases = 1
+        print("Note: num_biases was less than 1, and was thus set to 1.")
+        if coupler_bias_min != 0.0:
+            print("Note: the coupler bias was thus set to 0.")
+            coupler_bias_min = 0.0
+    elif coupler_dc_port == []:
+        if num_biases != 1:
+            num_biases = 1
+            print("Note: num_biases was set to 1, since the coupler_port array was empty.")
+        if coupler_bias_min != 0.0:
+            print("Note: the coupler bias was set to 0, since the coupler_port array was empty.")
+            coupler_bias_min = 0.0
+    
+    ## Initial array declaration
     
     # Declare amplitude array for the coupler to be swept
     coupler_amp_arr = np.linspace(coupler_bias_min, coupler_bias_max, num_biases)
@@ -787,8 +819,19 @@ def find_f_ro12_sweep_coupler(
         pls.hardware.set_inv_sinc(control_port, 0)
         
         # Coupler port
-        pls.hardware.set_dac_current(coupler_dc_port, 40_500)
-        pls.hardware.set_inv_sinc(coupler_dc_port, 0)
+        if coupler_dc_port != []:
+            pls.hardware.set_dac_current(coupler_dc_port, 40_500)
+            pls.hardware.set_inv_sinc(coupler_dc_port, 0)
+        
+        
+        # Sanitise user-input time arguments
+        plo_clk_T = pls.get_clk_T() # Programmable logic clock period.
+        readout_duration  = int(round(readout_duration / plo_clk_T)) * plo_clk_T
+        sampling_duration = int(round(sampling_duration / plo_clk_T)) * plo_clk_T
+        readout_sampling_delay = int(round(readout_sampling_delay / plo_clk_T)) * plo_clk_T
+        repetition_delay = int(round(repetition_delay / plo_clk_T)) * plo_clk_T
+        control_duration_01 = int(round(control_duration_01 / plo_clk_T)) * plo_clk_T
+        added_delay_for_bias_tee = int(round(added_delay_for_bias_tee / plo_clk_T)) * plo_clk_T
         
         
         ''' Setup mixers '''
@@ -804,14 +847,15 @@ def find_f_ro12_sweep_coupler(
         pls.hardware.configure_mixer(
             freq      = control_freq_01,
             out_ports = control_port,
-            sync      = False,
+            sync      = (coupler_dc_port == []),
         )
         # Coupler port mixer
-        pls.hardware.configure_mixer(
-            freq      = 0.0,
-            out_ports = coupler_dc_port,
-            sync      = True,  # Sync here
-        )
+        if coupler_dc_port != []:
+            pls.hardware.configure_mixer(
+                freq      = 0.0,
+                out_ports = coupler_dc_port,
+                sync      = True,  # Sync here
+            )
         
         
         ''' Setup scale LUTs '''
@@ -829,11 +873,12 @@ def find_f_ro12_sweep_coupler(
             scales          = control_amp_01,
         )
         # Coupler bias amplitude (to be swept)
-        pls.setup_scale_lut(
-            output_ports    = coupler_dc_port,
-            group           = 0,
-            scales          = coupler_amp_arr,
-        )
+        if coupler_dc_port != []:
+            pls.setup_scale_lut(
+                output_ports    = coupler_dc_port,
+                group           = 0,
+                scales          = coupler_amp_arr,
+            )
         
         
         ### Setup readout pulse ###
@@ -894,25 +939,25 @@ def find_f_ro12_sweep_coupler(
         
         
         ### Setup pulse "coupler_bias_tone" ###
-
-        # Setup the coupler tone bias.
-        coupler_bias_tone = pls.setup_long_drive(
-            output_port = coupler_dc_port,
-            group       = 0,
-            duration    = added_delay_for_bias_tee,
-            amplitude   = 1.0,
-            amplitude_q = 1.0,
-            rise_time   = 0e-9,
-            fall_time   = 0e-9
-        )
-        # Setup coupler bias tone "carrier"
-        pls.setup_freq_lut(
-            output_ports = coupler_dc_port,
-            group        = 0,
-            frequencies  = 0.0,
-            phases       = 0.0,
-            phases_q     = 0.0,
-        )
+        if coupler_dc_port != []:
+            # Setup the coupler tone bias.
+            coupler_bias_tone = [pls.setup_long_drive(
+                output_port = _port,
+                group       = 0,
+                duration    = added_delay_for_bias_tee,
+                amplitude   = 1.0,
+                amplitude_q = 1.0,
+                rise_time   = 0e-9,
+                fall_time   = 0e-9
+            ) for _port in coupler_dc_port]
+            # Setup coupler bias tone "carrier"
+            pls.setup_freq_lut(
+                output_ports = coupler_dc_port,
+                group        = 0,
+                frequencies  = 0.0,
+                phases       = 0.0,
+                phases_q     = 0.0,
+            )
         
         ### Setup sampling window ###
         pls.set_store_ports(readout_sampling_port)
@@ -927,19 +972,21 @@ def find_f_ro12_sweep_coupler(
         T = 0.0  # s
 
         # Charge the bias tee.
-        pls.reset_phase(T, coupler_dc_port)
-        pls.output_pulse(T, coupler_bias_tone)
-        T += added_delay_for_bias_tee
+        if coupler_dc_port != []:
+            pls.reset_phase(T, coupler_dc_port)
+            pls.output_pulse(T, coupler_bias_tone)
+            T += added_delay_for_bias_tee
         
-        # Redefine the coupler DC pulse duration for repeated playback
-        # once one tee risetime has passed.
-        coupler_bias_tone.set_total_duration(readout_duration + repetition_delay)
-        
+            # Redefine the coupler DC pulse duration for repeated playback
+            # once one tee risetime has passed.
+            coupler_bias_tone.set_total_duration(readout_duration + repetition_delay)
+            
         # For every resonator stimulus pulse frequency to sweep over:
         for ii in range(num_freqs):
 
             # Re-apply the coupler bias tone.
-            pls.output_pulse(T, coupler_bias_tone)
+            if coupler_dc_port != []:
+                pls.output_pulse(T, coupler_bias_tone)
             
             # Put the system into state |1>
             pls.reset_phase(T, control_port)
@@ -961,7 +1008,8 @@ def find_f_ro12_sweep_coupler(
         
         
         # Increment the coupler port's DC bias.
-        pls.next_scale(T, coupler_dc_port)
+        if coupler_dc_port != []:
+            pls.next_scale(T, coupler_dc_port)
         
         # Move to next iteration.
         T += repetition_delay
@@ -989,6 +1037,9 @@ def find_f_ro12_sweep_coupler(
         ''' SAVE AS LOG BROWSER COMPATIBLE HDF5 '''
         ###########################################
         
+        # Get timestamp for Log Browser exporter.
+        timestamp = (datetime.now()).strftime("%d-%b-%Y_(%H_%M_%S)")
+        
         # Data to be stored.
         hdf5_steps = [
             'readout_pulse_freq_arr', "Hz",
@@ -1009,6 +1060,7 @@ def find_f_ro12_sweep_coupler(
             'control_freq_01', "Hz",
             'control_duration_01', "s",
             
+            #'coupler_dc_port', "",
             'added_delay_for_bias_tee', "s",
             'num_freqs', "",
             'num_averages', "",
@@ -1016,88 +1068,102 @@ def find_f_ro12_sweep_coupler(
             'coupler_bias_min', "FS",
             'coupler_bias_max', "FS",
         ]
+        hdf5_logs = [
+            'fetched_data_arr', "FS",
+        ]
         
-        # Assert that every key bears a corresponding unit entered above.
+        # Assert that the received keys bear (an even number of) entries,
+        # implying whether a unit is missing.
         number_of_keyed_elements_is_even = \
-            ((len(hdf5_steps) % 2) == 0) and ((len(hdf5_singles) % 2) == 0)
+            ((len(hdf5_steps) % 2) == 0) and \
+            ((len(hdf5_singles) % 2) == 0) and \
+            ((len(hdf5_logs) % 2) == 0)
         assert number_of_keyed_elements_is_even, "Error: non-even amount "  + \
-            "of keys and units entered in the portion of the measurement "  + \
-            "script that saves the data. Someone likely forgot a comma."
+            "of keys and units provided. Someone likely forgot a comma."
         
-        # Build step lists
+        # Stylistically rework underscored characters in the axes dict.
+        for axis in ['x_name','x_unit','y_name','y_unit','z_name','z_unit']:
+            axes[axis] = axes[axis].replace('/2','/₂')
+            axes[axis] = axes[axis].replace('/3','/₃')
+            axes[axis] = axes[axis].replace('_01','₀₁')
+            axes[axis] = axes[axis].replace('_02','₀₂')
+            axes[axis] = axes[axis].replace('_03','₀₃')
+            axes[axis] = axes[axis].replace('_12','₁₂')
+            axes[axis] = axes[axis].replace('_13','₁₃')
+            axes[axis] = axes[axis].replace('_23','₂₃')
+            axes[axis] = axes[axis].replace('_0','₀')
+            axes[axis] = axes[axis].replace('_1','₁')
+            axes[axis] = axes[axis].replace('_2','₂')
+            axes[axis] = axes[axis].replace('_3','₃')
+        
+        # Build step lists, re-scale and re-unit where necessary.
         ext_keys = []
         for ii in range(0,len(hdf5_steps),2):
             if (hdf5_steps[ii] != 'fetched_data_arr') and (hdf5_steps[ii] != 'time_matrix'):
+                temp_name   = hdf5_steps[ii]
                 temp_object = np.array( eval(hdf5_steps[ii]) )
-                ext_keys.append(dict(name=hdf5_steps[ii], unit=hdf5_steps[ii+1], values=temp_object))
+                temp_unit   = hdf5_steps[ii+1]
+                if ii == 0:
+                    if (axes['x_name']).lower() != 'default':
+                        # Replace the x-axis name
+                        temp_name = axes['x_name']
+                    if axes['x_scaler'] != 1.0:
+                        # Re-scale the x-axis
+                        temp_object *= axes['x_scaler']
+                    if (axes['x_unit']).lower() != 'default':
+                        # Change the unit on the x-axis
+                        temp_unit = axes['x_unit']
+                elif ii == 2:
+                    if (axes['z_name']).lower() != 'default':
+                        # Replace the z-axis name
+                        temp_name = axes['z_name']
+                    if axes['z_scaler'] != 1.0:
+                        # Re-scale the z-axis
+                        temp_object *= axes['z_scaler']
+                    if (axes['z_unit']).lower() != 'default':
+                        # Change the unit on the z-axis
+                        temp_unit = axes['z_unit']
+                ext_keys.append(dict(name=temp_name, unit=temp_unit, values=temp_object))
         for jj in range(0,len(hdf5_singles),2):
             if (hdf5_singles[jj] != 'fetched_data_arr') and (hdf5_singles[jj] != 'time_matrix'):
                 temp_object = np.array( [eval(hdf5_singles[jj])] )
                 ext_keys.append(dict(name=hdf5_singles[jj], unit=hdf5_singles[jj+1], values=temp_object))
+        log_dict_list = []
+        for kk in range(0,len(hdf5_logs),2):
+            log_entry_name = hdf5_logs[kk]
+            temp_log_unit = hdf5_logs[kk+1]
+            if (axes['y_name']).lower() != 'default':
+                # Replace the y-axis name
+                log_entry_name = axes['y_name']
+            if axes['y_scaler'] != 1.0:
+                # Re-scale the y-axis
+                ## NOTE! Direct manipulation of the fetched_data_arr array!
+                fetched_data_arr *= axes['y_scaler']   
+            if (axes['y_unit']).lower() != 'default':
+                # Change the unit on the y-axis
+                temp_log_unit = axes['y_unit']
+            log_dict_list.append(dict(name=log_entry_name, unit=temp_log_unit, vector=False, complex=save_complex_data))
         
-        # Get name and time for logfile.
-        path_to_script = os.path.realpath(__file__)  # Full path of current script
-        current_dir, name_of_running_script = os.path.split(path_to_script)
-        script_filename = os.path.splitext(name_of_running_script)[0]  # Name of current script
-        timestamp = (datetime.now()).strftime("%d-%b-%Y_(%H_%M_%S)") # Date and time
-        savefile_string = script_filename + '_12_' + timestamp + '.hdf5'  # Name of save file
-        save_path = os.path.join(current_dir, "data", savefile_string)  # Full path of save file
-        
-        # Make logfile
-        log_dict_list = [dict(name='fetched_data_arr', vector=False, complex=save_complex_data)]# , dict(name='time_matrix', vector=True)]
-        f = Labber.createLogFile_ForData(savefile_string, log_dict_list, step_channels=ext_keys, use_database = False)
-        
-        # Set project name, tag, and user in logfile.
-        f.setProject(script_filename)
-        f.setTags('krizan')
-        f.setUser('Christian Križan')
-        
-        # Split fetched_data_arr into repeats:
-        # fetched_data_arr SHAPE: num_stores * repeat_count, num_ports, smpls_per_store
-        integration_window_start = 1500 * 1e-9
-        integration_window_stop  = 2000 * 1e-9
-        t_span = integration_window_stop - integration_window_start
-        
-        # Get index corresponding to integration_window_start and integration_window_stop respectively
-        integration_start_index = np.argmin(np.abs(time_matrix - integration_window_start))
-        integration_stop_index = np.argmin(np.abs(time_matrix - integration_window_stop))
-        integr_indices = np.arange(integration_start_index, integration_stop_index)
-        
-        # Construct a matrix, where every row is an integrated sampling
-        # sequence corresponding to exactly one bias point.
-        
-        if(save_complex_data):
-            angles = np.angle(fetched_data_arr[:, 0, integr_indices], deg=False)
-            rows, cols = np.shape(angles)
-            for row in range(rows):
-                for col in range(cols):
-                    #if angles[row][col] < 0.0:
-                    angles[row][col] += (2.0 * np.pi)
-            angles_mean = np.mean(angles, axis=-1)
-            mean_len = len(angles_mean)
-            for row in range(mean_len):
-                angles_mean[row] -= (2.0 * np.pi)
-            processing_arr = np.mean(np.abs(fetched_data_arr[:, 0, integr_indices]), axis=-1) * np.exp(1j * angles_mean)
-            ##processing_arr = np.mean(np.abs(fetched_data_arr[:, 0, integr_indices]), axis=-1) * np.exp(1j * np.mean(angles, axis=-1))
-            ## processing_arr = np.mean(np.abs(fetched_data_arr[:, 0, integr_indices]), axis=-1) * np.exp(1j * np.mean(np.angle(fetched_data_arr[:, 0, integr_indices]), axis=-1))
-        else:
-            processing_arr = np.mean(np.abs(fetched_data_arr[:, 0, integr_indices]), axis=-1)
-        
-        # Reshape depending on the repeat variable, as well as the inner loop
-        # of the sequencer program.
-        processing_arr.shape = (num_biases, num_freqs)
-        
-        for i in range(num_biases):
-            f.addEntry( {"fetched_data_arr": processing_arr[i,:]} )
-        # TODO: "time_matrix does not exist."
-        #f.addEntry( {"time_matrix": time_matrix} )
-        
-        # Check if the hdf5 file was created in the local directory.
-        # If so, move it to the 'data' directory.
-        if os.path.isfile(os.path.join(current_dir, savefile_string)):
-            shutil.move( os.path.join(current_dir, savefile_string) , os.path.join(current_dir, "data", savefile_string))
-
-        # Print final success message.
-        print("Data saved, see " + save_path)
-           
-
+        # Save data!
+        save(
+            timestamp = timestamp,
+            ext_keys = ext_keys,
+            log_dict_list = log_dict_list,
+            
+            time_matrix = time_matrix,
+            fetched_data_arr = fetched_data_arr,
+            resonator_freq_if_arrays_to_fft = [],
+            
+            path_to_script = os.path.realpath(__file__),
+            use_log_browser_database = use_log_browser_database,
+            
+            integration_window_start = integration_window_start,
+            integration_window_stop = integration_window_stop,
+            inner_loop_size = num_freqs,
+            outer_loop_size = num_biases,
+            
+            save_complex_data = save_complex_data,
+            append_to_log_name_before_timestamp = '12',
+            append_to_log_name_after_timestamp  = '',
+            select_resonator_for_single_log_export = '',
+        )
