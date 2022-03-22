a644df9cc769c313711d6e4ed6725e4640da8022
Christian Križan <christian.krizan@gmail.com>
2022-03-22 14:01:27 +0100

    Added capability of finding area, and hamiltonian path from real-imaginary data.
    Also, some minor bug fixes have been added to the data_discriminator.py file.

--- data_discriminator.py
+++ data_discriminator.py
@@ -32,10 +32,11 @@ def initiate_discriminator_settings_file(
     for ii in range(no_resonators_qubit_pairs):
         json_dict.update(
             {'resonator_'+str(ii): 
-                {'readout_state_0':
+                {'qubit_states':
                     dict( qubit_state_0_centre_real = 0, qubit_state_0_centre_imag = 0 )
                 }
             }
+            
         )
     
     # Build JSON file.
@@ -45,38 +46,33 @@ def initiate_discriminator_settings_file(
 
 def calculate_and_update_resonator_value(
     path_to_data,
-    resonator_transmon_pair = None,
-    readout_state = None,
+    resonator_transmon_pair = None
     ):
     ''' Update the discrimination.json file based on supplied data.
     '''
-    
-    # Get file paths.
-    full_path_to_discriminator_settings_json = \
-        (get_file_path_of_discrimination_json())[0]
     
     # Load the file at path_to_data.
     with h5py.File(os.path.abspath(path_to_data), 'r') as h5f:
         processing_volume = h5f["processed_data"][()]
         prepared_qubit_states = h5f["prepared_qubit_states"][()]
         
-        # Automatically establish what state was gathered?
-        if readout_state != None:
-            # No: use the user-provided readout state.
-            readout_state = int(readout_state)
-        else:
-            # Yes: then find out the state from the read file.
-            readout_state_found = False
-            readout_state_numerical = 0
-            for state_char in ['g','e','f','h']:
-                if not readout_state_found:
-                    try:
-                        readout_state_frequency = h5f.attrs["readout_freq_"+state_char+"_state"]
-                        readout_state_found = True
-                    except KeyError:
-                        readout_state_numerical += 1
-            assert readout_state_found, "Error: could not establish what state the readout was done in. States |g⟩, |e⟩, |f⟩, |h⟩ were considered."
-            readout_state = int(readout_state_numerical)
+        ## # Automatically establish what state was gathered?
+        ## if readout_state != None:
+        ##     # No: use the user-provided readout state.
+        ##     readout_state = int(readout_state)
+        ## else:
+        ##     # Yes: then find out the state from the read file.
+        ##     readout_state_found = False
+        ##     readout_state_numerical = 0
+        ##     for state_char in ['g','e','f','h']:
+        ##         if not readout_state_found:
+        ##             try:
+        ##                 readout_state_frequency = h5f.attrs["readout_freq_"+state_char+"_state"]
+        ##                 readout_state_found = True
+        ##             except KeyError:
+        ##                 readout_state_numerical += 1
+        ##     assert readout_state_found, "Error: could not establish what state the readout was done in. States |g⟩, |e⟩, |f⟩, |h⟩ were considered."
+        ##     readout_state = int(readout_state_numerical)
         ''' TODO: Make sure that when collecting IQ plane data,
         to what extent does it matter that the IQ data for the individual
         state centres have been collected using single-resonator readout?
@@ -113,7 +109,274 @@ def calculate_and_update_resonator_value(
         centre.append( np.mean(curr_data) )
     
     # Get JSON.
-    with open(full_path_to_discriminator_settings_json, "r") as fjson:
+    loaded_json_data = load_discriminator_settings()
+    
+    # Save this centre for this state X for resonator Y in the JSON.
+    # Update missing keys if any.
+    for jj in range(len(prepared_qubit_states)):
+        # The key might be non-existant. Then we may do whatever we want.
+        try:
+            curr_dict = (loaded_json_data['resonator_'+str(resonator_transmon_pair)])['qubit_states']
+            # The key existed. Then update. Remember that the key will update.
+            # There is no need to re-save data into the loaded_json_data array.
+            curr_dict.update( \
+                { \
+                    'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_real' : centre[jj].real, \
+                    'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_imag' : centre[jj].imag  \
+                }
+            )
+        except KeyError:
+            ##top_dict = loaded_json_data['resonator_'+str(resonator_transmon_pair)]
+            ##top_dict.update( \
+            ##    {'readout_state_'+str(readout_state) : \
+            top_dict = loaded_json_data
+            top_dict.update(
+                {'resonator_'+str(resonator_transmon_pair) : \
+                    {'qubit_states' : \
+                        {
+                            'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_real' : centre[jj].real, \
+                            'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_imag' : centre[jj].imag  \
+                        }
+                    }
+                }
+            )
+    
+    # Save updated JSON.
+    save_discriminator_settings(loaded_json_data)
+    
+    # Also get the area (and more) spanned by the qubit states for this resonator.
+    find_area_and_mean_distance_between_all_qubit_states(resonator_transmon_pair)
+    
+
+def discriminate(
+    readout_resonator_ids = [],
+    ):
+    ''' Takes a data set and discriminates the data held within.
+    '''
+    
+    # Input sanitisation.
+    assert (len(readout_resonator_ids) != 0) and (len(intended_qubit_states_in_readout) != 0), print("TODO!")
+    ## TODO make sure that len(readout_resonator_ids) == len(intended_qubit_states_in_readout) AND that both are NOT(empty).
+    
+    # Check if the discriminator settings JSON exists.
+    # If not, throw an error.
+    ## TODO stuff.
+    
+    # The JSON exists. Based on the user-provided settings TODO
+    
+    assert 1 == 0, "Not finished"
+    
+    
+def find_area_and_mean_distance_between_all_qubit_states(
+    resonator_transmon_pair
+    ):
+    ''' Acquire the area and perimeter spanned by the readout states
+        for a given resonator. Also, update the JSON settings file.
+        Area * perimeter will typically have the unit cubic volt [V^3].
+    '''
+    
+    # Get JSON.
+    loaded_json_data = load_discriminator_settings()
+    
+    # For all qubit states in the readout, gather area and perimeter
+    # where possible.
+    area_spanned_by_qubit_states = []
+    mean_distance_between_all_states = []
+    hamiltonian_path_perimeter = []
+    
+    # Check the number of complex coordinates provided.
+    resonator_dict = loaded_json_data['resonator_'+str(resonator_transmon_pair)]
+    curr_investigated_dict = (resonator_dict)['qubit_states']
+    no_qubit_states = len(curr_investigated_dict) // 2
+    
+    # Check whether the program can find and area and a mean distance
+    # between all states given the acquired data.
+    curr_area_spanned_by_qubit_states = None
+    curr_mean_distance_between_all_states = None
+    curr_hamiltonian_path_perimeter = None
+    if no_qubit_states <= 2:
+        if no_qubit_states <= 1:
+            # If there are <= 1 states to look at, then one can not span
+            # an area for the readout done in |n>. Nor a perimeter.
+            print( \
+                "Warning: the discriminator_settings.json file for resonator "   +\
+                str(resonator_transmon_pair)+" does not contain a sufficient "   +\
+                "number of qubit states for calculating an area nor a mean "     +\
+                "distance between all states (№ states = "                       +\
+                str(no_qubit_states) + "). Returning \"None\" and \"None\".")
+        else:
+            # If there are 2 states to look at, then at least a vector
+            # can be acquired giving the length between the two states.
+            print( \
+                "Warning: the discriminator_settings.json file for resonator "   +\
+                str(resonator_transmon_pair)+" only contains a sufficient "      +\
+                "number of states for calculating a mean distance between "      +\
+                "two states (№ states = "+str(no_qubit_states)+"). The "         +\
+                "returned area will be \"None\".")
+            curr_mean_distance_between_all_states = -1
+    else:
+        # We have a sufficient number of states acquired. Remove the None
+        # status to signal down below that the area and mean state distance
+        # will be calculated.
+        curr_area_spanned_by_qubit_states = -1
+        curr_mean_distance_between_all_states = -1
+    
+    # TODO: Currently, this script does not calculate the Hamiltonian path
+    # for all states, other than for the simplest possible scenario
+    # (n points along the hamiltonian path = 3)
+    if no_qubit_states == 3:
+        curr_hamiltonian_path_perimeter = -1
+    elif no_qubit_states > 0:
+        print("WARNING: Hamiltonian path algorithm not implemented. Will only return a perimeter if № states = 3.") # TODO
+    
+    # Get all vertices of the polygon spanned in
+    # the real-imaginary plane.
+    vertices_x_arr = []
+    vertices_y_arr = []
+    vertices = np.array([])
+    for pp in range( no_qubit_states ):
+        vertices_x_arr.append(curr_investigated_dict['qubit_state_'+str(pp)+'_centre_real'])
+        vertices_y_arr.append(curr_investigated_dict['qubit_state_'+str(pp)+'_centre_imag'])
+        vertices = np.append( vertices, (vertices_x_arr[pp] + vertices_y_arr[pp]*1j) )
+    
+    # Should a spanned area be calculated?
+    if curr_area_spanned_by_qubit_states != None:
+        
+        # Implement jacobian trapezoidal formula to get the area.
+        def jacobian_trapezoidal_area(x_list,y_list):
+            a1,a2 = 0,0
+            x_list.append(x_list[0])
+            y_list.append(y_list[0])
+            for j in range(len(x_list)-1):
+                a1 += x_list[j]*y_list[j+1]
+                a2 += y_list[j]*x_list[j+1]
+            l = abs(a1-a2)/2
+            return l
+        curr_area_spanned_by_qubit_states = \
+            jacobian_trapezoidal_area(vertices_x_arr, vertices_y_arr)
+    
+    # Should a mean distance between all readout states be calculated?
+    if mean_distance_between_all_states != None:
+        # Set up running mean. All state distances are summed together
+        # exactly once, and the total sum is divided with the
+        # number of considered states.
+        uu = len(vertices)
+        if uu > 1:
+            mean_sum     = 0
+            mean_divisor = 0
+            while (uu-1) > 0:
+                for rr in range(uu-1):
+                    mean_sum     += np.abs(vertices[uu-1]-vertices[rr])
+                    mean_divisor += 1
+                uu -= 1 # Subtract reciprocal counter
+            # Perform final mean divison.
+            curr_mean_distance_between_all_states = mean_sum / mean_divisor
+        else:
+            # There are less than two states considered,
+            # we may thus not calculate a distance.
+            curr_mean_distance_between_all_states = None
+    
+    # Should a Hamiltonian path be drawn between all vertices
+    # to calculate the shortest possible perimeter that spans all states?
+    if curr_hamiltonian_path_perimeter != None:
+        # TODO! Not the Hamiltonian path algorithm
+        curr_hamiltonian_path_perimeter = \
+            np.abs(vertices[2] - vertices[1]) +\
+            np.abs(vertices[1] - vertices[0]) +\
+            np.abs(vertices[0] - vertices[2])
+    
+    # Update the return values.
+    area_spanned_by_qubit_states.append(curr_area_spanned_by_qubit_states)
+    mean_distance_between_all_states.append(curr_mean_distance_between_all_states)
+    hamiltonian_path_perimeter.append(curr_hamiltonian_path_perimeter)
+    
+    # Update the local JSON that will be saved to file.
+    try:
+        analysis_dict = resonator_dict['analysis']
+    except KeyError:
+        resonator_dict.update( \
+            {'analysis': {} }  \
+        )
+        analysis_dict = resonator_dict['analysis']
+    if curr_area_spanned_by_qubit_states != None:
+        analysis_dict.update( \
+            {'area_spanned_by_qubit_states': curr_area_spanned_by_qubit_states} \
+        )
+    else:
+        try:
+            del analysis_dict['area_spanned_by_qubit_states']
+        except KeyError:
+            # The key did not exist, this is OK.
+            pass
+    if curr_mean_distance_between_all_states != None:
+        analysis_dict.update( \
+            {'mean_distance_between_all_states': curr_mean_distance_between_all_states} \
+        )
+    else:
+        try:
+            del analysis_dict['mean_distance_between_all_states']
+        except KeyError:
+            # The key did not exist, this is OK.
+            pass
+    if curr_hamiltonian_path_perimeter != None:
+        analysis_dict.update( \
+            {'hamiltonian_path_perimeter': curr_hamiltonian_path_perimeter} \
+        )
+    else:
+        try:
+            del analysis_dict['hamiltonian_path_perimeter']
+        except KeyError:
+            # The key did not exist, this is OK.
+            pass
+    
+    # Save calculated settings
+    save_discriminator_settings(loaded_json_data)
+    
+    return area_spanned_by_qubit_states, mean_distance_between_all_states, hamiltonian_path_perimeter
+    
+    
+def get_file_path_of_discrimination_json():
+    ''' Get name, file path for discriminator script.
+    '''
+    # Go!
+    path_to_discriminator = __file__
+    if not ('QPU interfaces' in path_to_discriminator):
+        raise OSError("The provided path to the discriminator settings file did not include a 'Core parameters' folder. A discriminator settings file was not created.")
+    call_root = path_to_discriminator.split('QPU interfaces',1)[0]
+    not_call_root = path_to_discriminator.split('QPU interfaces',1)[1]
+    name_of_interface = not_call_root.split(os.sep)[1] # Note: the first entry will be a '' in the split-list.
+    
+    # Make path to the discriminator JSON, make a discriminator_settings.json file if missing.
+    discriminator_json_folder_path = os.path.join(call_root, 'Core parameters')
+    discriminator_json_folder_path = os.path.join(discriminator_json_folder_path, name_of_interface)
+    full_path_to_discriminator_settings_json = os.path.join(discriminator_json_folder_path,'discriminator_settings.json')
+    
+    return [full_path_to_discriminator_settings_json, call_root, name_of_interface]
+    
+def save_discriminator_settings(json_data = [], verbose = False):
+    ''' Update the discriminator_settings.json file.
+    '''
+    
+    # Get file path of the discriminator_settings.json file.
+    full_path = (get_file_path_of_discrimination_json())[0]
+    
+    if verbose:
+        print("Will start dumping JSON data to file \""+str(full_path)+"\"")
+    with open(full_path, "w") as fjson:
+        json.dump(json_data, fjson, indent=4)
+        fjson.close()
+    if verbose:
+        print("Finished dumping JSON data to file.")
+    
+    
+def load_discriminator_settings(verbose = False):
+    ''' Get the content of the discriminator_settings.json file.
+    '''
+    
+    # Get file path of the discriminator_settings.json file.
+    full_path = (get_file_path_of_discrimination_json())[0]
+    
+    with open(full_path, "r") as fjson:
         loaded_json_data = json.load(fjson)
         '''          .,,*/(#########%%%%%%%%%%((/***,,,*,,*,,,,,,,,,,,
                      .#####################%#%%%%/*,,...,,,,,,,,,,,,,,
@@ -147,50 +410,9 @@ def calculate_and_update_resonator_value(
         ///((#%%%%%%%%%%#                                  %%%%%%%%%%%
         ((#%%%%%%%%%%%%%%                JASON!           *%%%%%%%%'''
     
-    # Save this centre for this state X for resonator Y in the JSON.
-    # Update missing keys if any.
-    for jj in range(len(prepared_qubit_states)):
-        # The key might be non-existant. Then do whatever we want.
-        try:
-            curr_dict = (loaded_json_data['resonator_'+str(resonator_transmon_pair)])['readout_state_'+str(readout_state)]
-            # The key existed. Then update. Remember that the key will update.
-            # There is no need to re-save data into the loaded_json_data array.
-            curr_dict.update( \
-                { \
-                    'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_real' : centre[jj].real, \
-                    'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_imag' : centre[jj].imag  \
-                }
-            )
-        except KeyError:
-            top_dict = loaded_json_data['resonator_'+str(resonator_transmon_pair)]
-            top_dict.update( \
-                {'readout_state_'+str(readout_state) : \
-                    {
-                        'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_real' : centre[jj].real, \
-                        'qubit_state_'+str(prepared_qubit_states[jj])+'_centre_imag' : centre[jj].imag  \
-                    }
-                }
-            )
+    if verbose:
+        print("Loaded JSON data from file \""+str(full_path)+"\"")
     
-    # Save updated JSON.
-    with open(full_path_to_discriminator_settings_json, "w") as fjson:
-        json.dump(loaded_json_data, fjson, indent=4)
-    
-    
-def get_file_path_of_discrimination_json():
-    ''' Get name, file path for discriminator script.
-    '''
-    # Go!
-    path_to_discriminator = __file__
-    if not ('QPU interfaces' in path_to_discriminator):
-        raise OSError("The provided path to the discriminator settings file did not include a 'Core parameters' folder. A discriminator settings file was not created.")
-    call_root = path_to_discriminator.split('QPU interfaces',1)[0]
-    not_call_root = path_to_discriminator.split('QPU interfaces',1)[1]
-    name_of_interface = not_call_root.split(os.sep)[1] # Note: the first entry will be a '' in the split-list.
-    
-    # Make path to the discriminator JSON, make a discriminator_settings.json file if missing.
-    discriminator_json_folder_path = os.path.join(call_root, 'Core parameters')
-    discriminator_json_folder_path = os.path.join(discriminator_json_folder_path, name_of_interface)
-    full_path_to_discriminator_settings_json = os.path.join(discriminator_json_folder_path,'discriminator_settings.json')
-    
-    return [full_path_to_discriminator_settings_json, call_root, name_of_interface]
+    return loaded_json_data
+        
+        
