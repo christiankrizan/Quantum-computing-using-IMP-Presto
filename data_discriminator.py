Bug fix in state probability calculation.
The "n-ary" (binary, trinary) to decimal conversion in the data exporter would always be incorrect if the user's data did not happen to discriminate to the highest state present in the system. Example: if the user was looking for the state population of |01⟩, and had characterised the complex coordinate centre for state |2⟩ in the system, but no readout got state discriminated into |2⟩, then all of the state probability calculations for states |n0⟩, |n1⟩, and |n2⟩ would be invalid for all number strings n.

data_discriminator.py
@@ -155,7 +155,7 @@ def calculate_and_update_resonator_value(
     
 
 def discriminate(
-    get_probabilities_on_these_states,
+    states_that_will_be_checked,
     data_or_filepath_to_data,
     i_provided_a_filepath = False,
     ordered_resonator_ids_in_readout_data = []
@@ -195,7 +195,7 @@ def discriminate(
         
     # Assert that the user is not trying to look for states that are
     # unavailable for a particular resonator.
-    for curr_state_string in get_probabilities_on_these_states:
+    for curr_state_string in states_that_will_be_checked:
         for pp in range(len(curr_state_string)):
             # Check that this sought-for state is available in said resonator.
             for cc in range(len(states_present_for_resonators)):
@@ -208,23 +208,29 @@ def discriminate(
                     " resonator stored. Please configure state "             +\
                     "discrimination for this state on this resonator "       +\
                     "to continue."
-        pass #TODO HAER
     
     # Get data.
     if i_provided_a_filepath:
         # The user provided a filepath to data.
         assert isinstance(data_or_filepath_to_data, str), "Error: the discriminator was provided a non-string datatype. Expected string (filepath to data)."
         with h5py.File(os.path.abspath(data_or_filepath_to_data), 'r') as h5f:
-            processed_data = h5f["processed_data"][()]
+            extracted_data = h5f["processed_data"][()]
     else:
         # Then the user provided the data raw.
-        assert (not isinstance(data_or_filepath_to_data, str)), "Error: the discriminator was provided a string type. Expected raw data. The provided variable was: "+str(processsed_data)
-        processed_data = data_or_filepath_to_data
+        assert (not isinstance(data_or_filepath_to_data, str)), "Error: the discriminator was provided a string type. Expected raw data. The provided variable was: "+str(extracted_data)
+        extracted_data = data_or_filepath_to_data
     
-    # We now have the necessary data for discretisation, and the
-    # processed data that will be discretised. Let's go.
-    for current_res_idx in range(len(processed_data)):
-        res_content = processed_data[current_res_idx]
+    # Prepare the highest_state_in_system variable.
+    # As we will soon look at all states available in the system,
+    # we might as well keep track of the number of states in the system
+    # in the for loop below. num_states_in_system = highest_state_in_system +1
+    # Because |0> counts too.
+    highest_state_in_system = 0
+    
+    # We now have the discretisation information needed, and the
+    # processed data that will be discretised using the known state centres.
+    for current_res_idx in range(len(extracted_data)):
+        res_content = extracted_data[current_res_idx]
         states = (states_present_for_resonators[current_res_idx])[0]
         state_centres = np.array((states_present_for_resonators[current_res_idx])[1])
         
@@ -235,9 +241,18 @@ def discriminate(
             res_content[bb] = states[assigned]
         
         # Save and move on to the next resonator.
-        processed_data[current_res_idx] = res_content.astype(int)
+        extracted_data[current_res_idx] = res_content.astype(int)
+        
+        # For future reference (the return of this funtion), store the
+        # largest represented state in the system.
+        if np.max(states) > highest_state_in_system:
+            highest_state_in_system = np.max(states)
     
-    return processed_data
+    # The number of states in the system is the highest represented
+    # state in the system +1, because |0> counts.
+    num_states_in_system = highest_state_in_system + 1
+    
+    return extracted_data, num_states_in_system
     
     
 def find_area_and_mean_distance_between_all_qubit_states(

data_exporter.py
@@ -331,12 +331,18 @@ def save(
         # But, all of this data is given on a single line in a vector.
         
         # First, discretise every entry in the in processed_data[:]
-        discriminated_data = discriminate(
-            get_probabilities_on_these_states = get_probabilities_on_these_states,
+        discriminated_data, num_states = discriminate(
+            states_that_will_be_checked = get_probabilities_on_these_states,
             data_or_filepath_to_data = processed_data,
             i_provided_a_filepath = False,
             ordered_resonator_ids_in_readout_data = ordered_resonator_ids_in_readout_data
         )
+        
+        ## TODO DEBUG
+        ## For whatever dumbfuck reason, running discriminated_data
+        ## changes processed_data so that it matches discriminated_data.
+        ## Should not be a problem I think. processed_data gets properly
+        ## blanked further down the code.
         
         # discriminated_data[resonator] now contains only discrete values.
         # We will reshape this into a 3D-volume with the following dimensions:
@@ -361,16 +367,7 @@ def save(
         
         # We now want to look for probabilities of some user-provided states.
         # Remove duplicates from get_probabilities_on_these_states.
-        # get_probabilities_on_these_states will get some random order.
-        get_probabilities_on_these_states = list(set(get_probabilities_on_these_states))
-        
-        # Which is the highest represented state in the entire system?
-        num_states = 0
-        for item in discriminated_data:
-            highest_num_in_slice = np.max(item)
-            if highest_num_in_slice > num_states:
-                num_states = highest_num_in_slice
-        num_states = num_states +1 # Keep in mind that 0 counts (= |0>)
+        ## TODO Feature removed for now, since list(set( )) randomises everything.
         
         # Remake the discriminated data into an integer format.
         ''' Here is my way of representing n-qubit states with unique integers.
@@ -461,6 +458,23 @@ def save(
             fetch.shape = (outer_loop_size, inner_loop_size)
             processed_data[pv] = fetch
         
+        # Error-check that the probability calculation is correct.
+        # No "pixel" should have more than 100% probability.
+        # TODO: This segment can probably be removed some time in the future.
+        curr_pix_total = 0
+        for all_rows in range(len(processed_data[0])):
+            for all_cols in range(len((processed_data[0])[0])):
+                for all_states_checked in range(len(processed_data)):
+                    curr_pix_total += (processed_data[all_states_checked])[all_rows][all_cols]
+                # Check whether this pixel has more than 100% probability.
+                assert curr_pix_total <= 1.0, \
+                    "\nHalted! Pixel ("+str(all_rows)+","+str(str(all_cols)) +\
+                    ") returned more than 100% probability for its state"    +\
+                    " distribution. \n\nDebug information:\n"                +\
+                    "Pixel total was: " + str(curr_pix_total)+"\n\nInteger " +\
+                    "matrix:\n"+str(integer_rep_matrix)+"\n\nProbability-"   +\
+                    "vectors matrix:\n"+str(probability_vectors)
+                curr_pix_total = 0.0
     else:
         for mm in range(len(processed_data[:])):
             fetch = processed_data[mm]
@@ -484,7 +498,7 @@ def save(
                 
                 # Scale with some user-set scale and store.
                 processed_data[mm] = fetch * fetched_data_scale[mm]
-        
+    
     # Did the user request to flip the processed data?
     # I.e. that every new repeat in some measurement will be a column
     # instead of a row in the processed data? Then fix this.
@@ -631,24 +645,8 @@ def export_processed_data_to_file(
                         })
                     f.addEntry(dict_to_add_into_log) # This line stores data.
                     dict_to_add_into_log.clear()
-                
-                ''' Old code below.
-                if (len(resonator_freq_if_arrays_to_fft) > 1):
-                    ##if (len(processed_data) > 1):
-                    # Then store multiplexed! TODO: add support for n-length qubits' woth of data.
-                    # For every loop entry that is to be stored in this log:
-                    for loop_i in range(len( (processed_data[0])[:] )):
-                        f.addEntry({
-                            (log_dict_list[0])['name']: (processed_data[0])[loop_i, :],
-                            (log_dict_list[1])['name']: (processed_data[1])[loop_i, :]
-                        })
-                else:
-                    for loop_i in range(len( (processed_data[0])[:] )):
-                        f.addEntry({
-                            (log_dict_list[0])['name']: (processed_data[0])[loop_i, :]
-                        })'''
             else:
-                # TODO This else-case must be removed.
+                # TODO1: This else-case must be removed.
                 # TODO2: It can likely be removed if only looking at how long
                 #        processed_data is.
                 for log_i in range(len(log_dict_list[:])):