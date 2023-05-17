756fa9f068bf19bb02d3866feb875051850ff41b
Christian Križan <christian.krizan@gmail.com>
2023-05-17 12:05:23 +0200

    Update the DC biasing routines for thermal excitation measurements.
    The update also enables a fixed repetition rate for initial thermal excitation measurements.

--- thermal_population.py
+++ thermal_population.py
@@ -49,9 +49,8 @@ def acquire_thermal_population_from_12_rabi_ro1(
     
     control_port,
     control_freq_nco,
-    
-    control_amp_01,
     control_freq_01,
+    control_amp_01,
     control_duration_01,
     
     control_freq_12,
@@ -59,7 +58,7 @@ def acquire_thermal_population_from_12_rabi_ro1(
     
     coupler_dc_port,
     coupler_dc_bias,
-    added_delay_for_bias_tee,
+    settling_time_of_bias_tee,
     
     num_amplitudes,
     num_averages,
@@ -89,13 +88,13 @@ def acquire_thermal_population_from_12_rabi_ro1(
         "z_unit":   'default',
         }
     ):
-    ''' Note: the cited method calls for reading out in state |1>
+    ''' Note: the cited method calls for reading out in state |1⟩
         
-        Prepare |0>, then perform a |1> -> |2> Rabi oscillation.
-        Then prepare |1>, and do the same oscillation.
+        Prepare |0⟩, then perform a |1⟩ → |2⟩ Rabi oscillation.
+        Then prepare |1⟩, and do the same oscillation.
         The ratio between the 
         
-        Perform a Rabi oscillation experiment between states |1> and |2>.
+        Perform a Rabi oscillation experiment between states |1⟩ and |2⟩.
         The energy is found by sweeping the amplitude, instead of
         sweeping the pulse duration. While sweeping, a bias voltage
         can be applied onto a connected coupler.
@@ -105,8 +104,6 @@ def acquire_thermal_population_from_12_rabi_ro1(
         The method follows the measurement methodology as shown in figure 1
         of https://journals.aps.org/prl/pdf/10.1103/PhysRevLett.114.240501
     '''
-    
-    assert 1 == 0, "Halted! This function's DC biasing has not been modernised."
     
     ## Input sanitisation
     
@@ -148,20 +145,24 @@ def acquire_thermal_population_from_12_rabi_ro1(
         pls.hardware.set_dac_current(control_port, 40_500)
         pls.hardware.set_inv_sinc(control_port, 0)
         
-        # Coupler port(s)
+        # Configure the DC bias. Also, let's charge the bias-tee.
         if coupler_dc_port != []:
-            pls.hardware.set_dac_current(coupler_dc_port, 40_500)
-            pls.hardware.set_inv_sinc(coupler_dc_port, 0)
+            pls.hardware.set_dc_bias( \
+                coupler_dc_bias, \
+                coupler_dc_port, \
+                range_i = get_dc_dac_range_integer(coupler_dc_bias) \
+            )
+            time.sleep( settling_time_of_bias_tee )
         
         # Sanitise user-input time arguments
         plo_clk_T = pls.get_clk_T() # Programmable logic clock period.
         readout_duration  = int(round(readout_duration / plo_clk_T)) * plo_clk_T
         sampling_duration = int(round(sampling_duration / plo_clk_T)) * plo_clk_T
         readout_sampling_delay = int(round(readout_sampling_delay / plo_clk_T)) * plo_clk_T
-        repetition_delay = int(round(repetition_delay / plo_clk_T)) * plo_clk_T
+        repetition_rate = int(round(repetition_rate / plo_clk_T)) * plo_clk_T
         control_duration_01 = int(round(control_duration_01 / plo_clk_T)) * plo_clk_T
         control_duration_12 = int(round(control_duration_12 / plo_clk_T)) * plo_clk_T
-        added_delay_for_bias_tee = int(round(added_delay_for_bias_tee / plo_clk_T)) * plo_clk_T
+        settling_time_of_bias_tee = int(round(settling_time_of_bias_tee / plo_clk_T)) * plo_clk_T
         
         # Check whether the integration window is legal.
         integration_window_stop = check_if_integration_window_is_legal(
@@ -186,18 +187,8 @@ def acquire_thermal_population_from_12_rabi_ro1(
             freq      = control_freq_nco,
             out_ports = control_port,
             tune      = True,
-            sync      = (coupler_dc_port == []),
+            sync      = True,
         )
-        # Coupler port mixer
-        if coupler_dc_port != []:
-            for curr_coupler_dc_port in range(len(coupler_dc_port)):
-                pls.hardware.configure_mixer(
-                    freq      = 0.0,
-                    out_ports = coupler_dc_port,
-                    tune      = True,
-                    sync      = curr_coupler_dc_port == (len(coupler_dc_port)-1),
-                )
-        
 
         ''' Setup scale LUTs '''
         
@@ -218,14 +209,6 @@ def acquire_thermal_population_from_12_rabi_ro1(
             group           = 1,
             scales          = control_amp_arr,
         )
-        # Coupler port amplitude (the bias)
-        if coupler_dc_port != []:
-            pls.setup_scale_lut(
-                output_ports    = coupler_dc_port,
-                group           = 0,
-                scales          = coupler_dc_bias,
-            )
-        
         
         ### Setup readout pulse ###
         
@@ -292,29 +275,6 @@ def acquire_thermal_population_from_12_rabi_ro1(
             phases_q     = bandsign(control_freq_if_12),
         )
         
-        
-        ### Setup pulse "coupler_bias_tone" ###
-        if coupler_dc_port != []:
-            # Setup the coupler tone bias.
-            coupler_bias_tone = [pls.setup_long_drive(
-                output_port = _port,
-                group       = 0,
-                duration    = added_delay_for_bias_tee,
-                amplitude   = 1.0,
-                amplitude_q = 1.0,
-                rise_time   = 0e-9,
-                fall_time   = 0e-9
-            ) for _port in coupler_dc_port]
-        
-            # Setup coupler bias tone "carrier"
-            pls.setup_freq_lut(
-                output_ports = coupler_dc_port,
-                group        = 0,
-                frequencies  = 0.0,
-                phases       = 0.0,
-                phases_q     = 0.0,
-            )
-        
         ### Setup sampling window ###
         pls.set_store_ports(readout_sampling_port)
         pls.set_store_duration(sampling_duration)
@@ -327,11 +287,8 @@ def acquire_thermal_population_from_12_rabi_ro1(
         # Start of sequence
         T = 0.0 # s
         
-        # Charge the bias tee.
-        if coupler_dc_port != []:
-            pls.reset_phase(T, coupler_dc_port)
-            pls.output_pulse(T, coupler_bias_tone)
-            T += added_delay_for_bias_tee
+        # Define repetition counter for T.
+        repetition_counter = 1
         
         # Run |1⟩ → |2⟩ Rabi oscillations,
         # with and without the |1⟩ state as the input state.
@@ -340,32 +297,22 @@ def acquire_thermal_population_from_12_rabi_ro1(
             # For all amplitudes to sweep through.
             for ii in range(num_amplitudes):
                 
-                # Redefine the coupler DC pulse duration to keep on playing once
-                # the bias tee has charged.
-                if coupler_dc_port != []:
-                    for bias_tone in coupler_bias_tone:
-                        bias_tone.set_total_duration(
-                            control_duration_01 + \
-                            control_duration_12 + \
-                            readout_duration + \
-                            repetition_delay
-                        )
-                    
-                    # Re-apply the coupler bias tone.
-                    pls.output_pulse(T, coupler_bias_tone)
+                # Get a time reference, used for gauging the iteration length.
+                T_begin = T
                 
                 # Prepare for pulses on the control port. Prepare |1⟩ or not?
                 pls.reset_phase(T, control_port)
                 if prepare_excited_or_not == 1:
                     pls.output_pulse(T, control_pulse_pi_01)
                 
-                # Add delay for pulse, regardless of whether |1⟩ was prepared.
+                # Add delay for pulse, regardless of
+                # whether |1⟩ was prepared or not.
                 T += control_duration_01
                 
-                # Output the pi_12 pulse to be characterised.
+                # Drive Rabi oscillations between states |1⟩ and |2⟩.
                 pls.output_pulse(T, control_pulse_pi_12)
                 T += control_duration_12
-            
+                
                 # Commence readout in the excited state
                 pls.reset_phase(T, readout_stimulus_port)
                 pls.output_pulse(T, readout_pulse_excited)
@@ -375,8 +322,10 @@ def acquire_thermal_population_from_12_rabi_ro1(
                 # Move to next Rabi amplitude
                 pls.next_scale(T, control_port, group = 1)
                 
-                # Wait for decay
-                T += repetition_delay
+                # Get T that aligns with the repetition rate.
+                T, repetition_counter = get_repetition_rate_T(
+                    T_begin, T, repetition_rate, repetition_counter,
+                )
         
         ################################
         ''' EXPERIMENT EXECUTES HERE '''
@@ -389,6 +338,10 @@ def acquire_thermal_population_from_12_rabi_ro1(
             num_averages = num_averages,
             print_time   = True,
         )
+        
+        # Reset the DC bias port(s).
+        if (coupler_dc_port != []) and reset_dc_to_zero_when_finished:
+            pls.hardware.set_dc_bias(0.0, coupler_dc_port)
     
     # Declare path to whatever data will be saved.
     string_arr_to_return = []
@@ -415,7 +368,7 @@ def acquire_thermal_population_from_12_rabi_ro1(
             'sampling_duration', "s",
             
             'readout_sampling_delay', "s",
-            'repetition_delay', "s",
+            'repetition_rate', "s",
             'integration_window_start', "s",
             'integration_window_stop', "s",
             
@@ -431,7 +384,8 @@ def acquire_thermal_population_from_12_rabi_ro1(
             'control_duration_12', "s",
             
             #'coupler_dc_port', "",
-            'added_delay_for_bias_tee', "s",
+            'coupler_dc_bias', "V",
+            'settling_time_of_bias_tee', "s",
             
             'num_amplitudes', "",
             'num_averages', "",
