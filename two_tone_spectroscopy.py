d6cfc9e8e62a331fa77bff4eb8d931045c29a4a0
Christian Križan <christian.krizan@gmail.com>
2023-06-13 11:59:24 +0200

    Add bias sweep methods that force the DC bias sweep to be in the outer measurement loop.
    Due to inadequacies in the IMP API, using the biasing card on the Presto AWG lock-in amplifier while requiring the DC bias sweep to be done in the outer loop of the measurement, requires placing the entire measurement inside of one iteration of the sequencer. Meaning that the repeat functionality cannot be utilised. This is quite silly.

--- two_tone_spectroscopy.py
+++ two_tone_spectroscopy.py
@@ -1949,3 +1949,452 @@ def pulsed12_sweep_coupler(
         ))
         
     return string_arr_to_return
+
+def pulsed01_sweep_coupler_in_outer_measurement_loop(
+    ip_address,
+    ext_clk_present,
+    
+    readout_stimulus_port,
+    readout_sampling_port,
+    readout_freq_nco,
+    readout_freq,
+    readout_amp,
+    readout_duration,
+    
+    sampling_duration,
+    readout_sampling_delay,
+    repetition_rate,
+    integration_window_start,
+    integration_window_stop,
+
+    control_port,
+    control_amp_01,
+    control_duration_01,
+    control_freq_nco,
+    control_freq_01_centre,
+    control_freq_01_span,
+    
+    coupler_dc_port,
+    settling_time_of_bias_tee,
+    
+    num_freqs,
+    num_averages,
+    
+    num_biases,
+    coupler_bias_min = -0.0,
+    coupler_bias_max = +0.0,
+    
+    reset_dc_to_zero_when_finished = True,
+    
+    save_complex_data = True,
+    save_raw_time_data = False,
+    use_log_browser_database = True,
+    default_exported_log_file_name = 'default',
+    log_browser_tag  = 'default',
+    log_browser_user = 'default',
+    axes =  {
+        "x_name":   'default',
+        "x_scaler": 1.0,
+        "x_unit":   'default',
+        "y_name":   'default',
+        "y_scaler": [1.0],
+        "y_offset": [0.0],
+        "y_unit":   'default',
+        "z_name":   'default',
+        "z_scaler": 1.0,
+        "z_unit":   'default',
+        }
+    ):
+    ''' Find the optimal readout frequency for reading out qubits in state |0>,
+        as a function of a swept pairwise coupler bias.
+        
+        repetition_rate is the time multiple at which every single
+        measurement is repeated at. Example: a repetition rate of 300 µs
+        means that single iteration of a measurement ("a shot") begins anew
+        every 300 µs. If the measurement itself cannot fit into a 300 µs
+        window, then the next iteration will happen at the next integer
+        multiple of 300 µs.
+    '''
+    
+    ## Input sanitisation 
+    
+    # DC bias argument sanitisation.
+    coupler_bias_min, coupler_bias_max, num_biases, coupler_dc_bias, \
+    with_or_without_bias_string = sanitise_dc_bias_arguments(
+        coupler_dc_port  = coupler_dc_port,
+        coupler_bias_min = coupler_bias_min,
+        coupler_bias_max = coupler_bias_max,
+        num_biases       = num_biases,
+        coupler_dc_bias  = None
+    )
+    
+    # Sanitisation for whether the user has a
+    # span engaged but only a single frequency.
+    if ((num_freqs == 1) and (control_freq_01_span != 0.0)):
+        print("Note: single control frequency point requested, ignoring span parameter.")
+        control_freq_01_span = 0.0
+    
+    ## Initial array declaration
+    
+    # Declare amplitude array for the coupler to be swept
+    coupler_amp_arr = np.linspace(coupler_bias_min, coupler_bias_max, num_biases)
+    
+    
+    # Instantiate the interface
+    print("\nConnecting to "+str(ip_address)+"...")
+    with pulsed.Pulsed(
+        force_reload =   False,
+        address      =   ip_address,
+        ext_ref_clk  =   ext_clk_present,
+        adc_mode     =   AdcMode.Mixed,  # Use mixers for downconversion
+        adc_fsample  =   AdcFSample.G2,  # 2 GSa/s
+        dac_mode     =   [DacMode.Mixed42, DacMode.Mixed42, DacMode.Mixed02, DacMode.Mixed02],
+        dac_fsample  =   [DacFSample.G10, DacFSample.G10, DacFSample.G6, DacFSample.G6],
+        dry_run      =   False
+    ) as pls:
+        print("Connected. Setting up...")
+        
+        # Readout output and input ports
+        pls.hardware.set_adc_attenuation(readout_sampling_port, 0.0)
+        pls.hardware.set_dac_current(readout_stimulus_port, 40_500)
+        pls.hardware.set_inv_sinc(readout_stimulus_port, 0)
+        
+        # Control port
+        pls.hardware.set_dac_current(control_port, 40_500)
+        pls.hardware.set_inv_sinc(control_port, 0)
+        
+        # Configure the DC bias. Also, let's charge the bias-tee.
+        if coupler_dc_port != []:
+            pls.hardware.set_dc_bias( \
+                coupler_amp_arr[0], \
+                coupler_dc_port, \
+                range_i = get_dc_dac_range_integer(coupler_amp_arr) \
+            )
+            time.sleep( settling_time_of_bias_tee )
+        
+        # Sanitise user-input time arguments
+        plo_clk_T = pls.get_clk_T() # Programmable logic clock period.
+        readout_duration  = int(round(readout_duration / plo_clk_T)) * plo_clk_T
+        sampling_duration = int(round(sampling_duration / plo_clk_T)) * plo_clk_T
+        readout_sampling_delay = int(round(readout_sampling_delay / plo_clk_T)) * plo_clk_T
+        repetition_rate = int(round(repetition_rate / plo_clk_T)) * plo_clk_T
+        control_duration_01 = int(round(control_duration_01 / plo_clk_T)) * plo_clk_T
+        settling_time_of_bias_tee = int(round(settling_time_of_bias_tee / plo_clk_T)) * plo_clk_T
+        
+        # Check whether the integration window is legal.
+        integration_window_stop = check_if_integration_window_is_legal(
+            sample_rate = 1e9,
+            sampling_duration = sampling_duration,
+            integration_window_start = integration_window_start,
+            integration_window_stop  = integration_window_stop
+        )
+        
+        ''' Setup mixers '''
+        
+        # Readout port
+        pls.hardware.configure_mixer(
+            freq      = readout_freq_nco,
+            in_ports  = readout_sampling_port,
+            out_ports = readout_stimulus_port,
+            tune      = True,
+            sync      = False,
+        )
+        # Control port mixer
+        pls.hardware.configure_mixer(
+            freq      = control_freq_nco,
+            out_ports = control_port,
+            tune      = True,
+            sync      = True,
+        )
+        
+        ''' Setup scale LUTs '''
+        
+        # Readout port amplitude
+        pls.setup_scale_lut(
+            output_ports    = readout_stimulus_port,
+            group           = 0,
+            scales          = readout_amp,
+        )
+        # Control port amplitude
+        pls.setup_scale_lut(
+            output_ports    = control_port,
+            group           = 0,
+            scales          = control_amp_01,
+        )
+        
+        ### Setup readout pulse ###
+        
+        # Setup readout pulse envelope
+        readout_pulse = pls.setup_long_drive(
+            output_port = readout_stimulus_port,
+            group       = 0,
+            duration    = readout_duration,
+            amplitude   = 1.0,
+            amplitude_q = 1.0,
+            rise_time   = 0e-9,
+            fall_time   = 0e-9
+        )
+        # Setup readout carrier, considering that there is a digital mixer
+        readout_freq_if = readout_freq_nco - readout_freq
+        pls.setup_freq_lut(
+            output_ports = readout_stimulus_port,
+            group        = 0,
+            frequencies  = np.abs(readout_freq_if),
+            phases       = 0.0,
+            phases_q     = bandsign(readout_freq_if),
+        )
+
+        ### Setup pulse "control_pulse_pi_01" ###
+
+        # Setup control_pulse_pi_01 pulse envelope.
+        control_ns_01 = int(round(control_duration_01 * pls.get_fs("dac")))  # Number of samples in the control template.
+        control_envelope_01 = sin2(control_ns_01)
+        control_pulse_pi_01 = pls.setup_template(
+            output_port = control_port,
+            group       = 0,
+            template    = control_envelope_01,
+            template_q  = control_envelope_01,
+            envelope    = True,
+        )
+        
+        # Setup control pulse carrier, this tone will be swept in frequency.
+        control_freq_01_centre_if = control_freq_nco - control_freq_01_centre
+        f_start = control_freq_01_centre_if - control_freq_01_span / 2
+        f_stop  = control_freq_01_centre_if + control_freq_01_span / 2
+        control_freq_01_if_arr = np.linspace(f_start, f_stop, num_freqs)
+        
+        # Use the appropriate side band.
+        control_pulse_01_freq_arr = control_freq_nco - control_freq_01_if_arr
+        
+        # Setup LUT
+        pls.setup_freq_lut(
+            output_ports    = control_port,
+            group           = 0,
+            frequencies     = np.abs(control_freq_01_if_arr),
+            phases          = np.full_like(control_freq_01_if_arr, 0.0),
+            phases_q        = bandsign(control_freq_01_if_arr),
+        )
+        
+        ### Setup sampling window ###
+        pls.set_store_ports(readout_sampling_port)
+        pls.set_store_duration(sampling_duration)
+        
+        
+        #################################
+        ''' PULSE SEQUENCE STARTS HERE'''
+        #################################
+
+        # Start of sequence
+        T = 0.0  # s
+        
+        # Define repetition counter for T.
+        repetition_counter = 1
+        
+        # For every pulse to sweep over:
+        for ii in range(len(coupler_amp_arr)):
+            
+            # For every frequency to sweep over:
+            for jj in range(num_freqs):
+                
+                # Get a time reference, used for gauging the iteration length.
+                T_begin = T
+                
+                # Apply the frequency-swept pi_01 pulse.
+                pls.reset_phase(T, control_port)
+                pls.output_pulse(T, control_pulse_pi_01)
+                T += control_duration_01
+                
+                # Commence readout
+                pls.reset_phase(T, readout_stimulus_port)
+                pls.output_pulse(T, readout_pulse)
+                pls.store(T + readout_sampling_delay) # Sampling window
+                T += readout_duration
+                
+                # Step the frequency array in a dumbfuck manner.
+                # Since, there is no way I can declare some sequencer-
+                # accesible register where I store the coupler_dc_bias
+                # values.
+                pls.next_frequency(T, control_port)
+                T += 20e-9 # Add some time for changing the frequency.
+                
+                # Is this NOT the last iteration?
+                if jj != num_freqs-1:
+                    # Then get a T that aligns with the repetition rate.
+                    # Otherwise, we'll change the DC bias, see end of ii loop.
+                    T, repetition_counter = get_repetition_rate_T(
+                        T_begin, T, repetition_rate, repetition_counter,
+                    )
+            
+            # Step the DC bias to the next point.
+            if coupler_dc_port != []:
+                T = change_dc_bias(pls, T, coupler_amp_arr[ii+1], coupler_dc_port)
+                T += settling_time_of_bias_tee
+            
+            # Get T that aligns with the repetition rate.
+            T, repetition_counter = get_repetition_rate_T(
+                T_begin, T, repetition_rate, repetition_counter,
+            )
+        
+        ################################
+        ''' EXPERIMENT EXECUTES HERE '''
+        ################################
+
+        # Average the measurement over 'num_averages' averages
+        pls.run(
+            period          =   T,
+            repeat_count    =   1, # Cannot use this repeat factor, very silly.
+            num_averages    =   num_averages,
+            print_time      =   True,
+        )
+        
+        # Reset the DC bias port(s).
+        if (coupler_dc_port != []) and reset_dc_to_zero_when_finished:
+            pls.hardware.set_dc_bias(0.0, coupler_dc_port)
+    
+    # Declare path to whatever data will be saved.
+    string_arr_to_return = []
+    
+    if not pls.dry_run:
+        time_vector, fetched_data_arr = pls.get_store_data()
+        print("Raw data downloaded to PC.")
+
+        ###########################################
+        ''' SAVE AS LOG BROWSER COMPATIBLE HDF5 '''
+        ###########################################
+        
+        # Data to be stored.
+        hdf5_steps = [
+            'control_pulse_01_freq_arr', "Hz",
+            'coupler_amp_arr', "V",
+        ]
+        hdf5_singles = [
+            'readout_stimulus_port', "",
+            'readout_sampling_port', "",
+            
+            'readout_freq_nco', "Hz",
+            'readout_freq', "Hz",
+            'readout_amp', "FS",
+            'readout_duration', "s",
+            
+            'sampling_duration', "s",
+            'readout_sampling_delay', "s",
+            'repetition_rate', "s",
+            
+            'control_port', "",
+            'control_amp_01', "FS",
+            'control_duration_01', "s",
+            'control_freq_nco', "Hz",
+            'control_freq_01_centre', "Hz",
+            'control_freq_01_span', "Hz",
+            
+            #'coupler_dc_port', "",
+            'settling_time_of_bias_tee', "s",
+            
+            'num_freqs', "",
+            'num_averages', "",
+            
+            'num_biases', "",
+            'coupler_bias_min', "V",
+            'coupler_bias_max', "V",
+        ]
+        hdf5_logs = []
+        try:
+            if len(states_to_discriminate_between) > 0:
+                for statep in states_to_discriminate_between:
+                    hdf5_logs.append('Probability for state |'+statep+'⟩')
+                    hdf5_logs.append("")
+            save_complex_data = False
+        except NameError:
+            pass # Fine, no state discrimnation.
+        if len(hdf5_logs) == 0:
+            hdf5_logs = [
+                'fetched_data_arr', "FS",
+            ]
+        
+        # Ensure the keyed elements above are valid.
+        assert ensure_all_keyed_elements_even(hdf5_steps, hdf5_singles, hdf5_logs), \
+            "Error: non-even amount of keys and units provided. " + \
+            "Someone likely forgot a comma."
+        
+        # Stylistically rework underscored characters in the axes dict.
+        axes = stylise_axes(axes)
+        
+        # Create step lists
+        ext_keys = []
+        for ii in range(0,len(hdf5_steps),2):
+            ext_keys.append( get_dict_for_step_list(
+                step_entry_name   = hdf5_steps[ii],
+                step_entry_object = np.array( eval(hdf5_steps[ii]) ),
+                step_entry_unit   = hdf5_steps[ii+1],
+                axes = axes,
+                axis_parameter = ('x' if (ii == 0) else 'z' if (ii == 2) else ''),
+            ))
+        for jj in range(0,len(hdf5_singles),2):
+            ext_keys.append( get_dict_for_step_list(
+                step_entry_name   = hdf5_singles[jj],
+                step_entry_object = np.array( [eval(hdf5_singles[jj])] ),
+                step_entry_unit   = hdf5_singles[jj+1],
+            ))
+        for qq in range(len(axes['y_scaler'])):
+            if (axes['y_scaler'])[qq] != 1.0:
+                ext_keys.append(dict(name='Y-axis scaler for Y'+str(qq+1), unit='', values=(axes['y_scaler'])[qq]))
+            if (axes['y_offset'])[qq] != 0.0:
+                try:
+                    ext_keys.append(dict(name='Y-axis offset for Y'+str(qq+1), unit=hdf5_logs[2*qq+1], values=(axes['y_offset'])[qq]))
+                except IndexError:
+                    # The user is likely stepping a multiplexed readout with seperate plot exports.
+                    if (axes['y_unit'])[qq] != 'default':
+                        print("Warning: an IndexError occured when setting the ext_key unit for Y"+str(qq+1)+". Falling back to the first log_list entry's unit ("+str(hdf5_logs[1])+").")
+                    else:
+                        print("Warning: an IndexError occured when setting the ext_key unit for Y"+str(qq+1)+". Falling back to the first log_list entry's unit ("+(axes['y_unit'])[qq]+").")
+                    ext_keys.append(dict(name='Y-axis offset for Y'+str(qq+1), unit=hdf5_logs[1], values=(axes['y_offset'])[qq]))
+        
+        # Create log lists
+        log_dict_list = []
+        for kk in range(0,len(hdf5_logs),2):
+            if (len(hdf5_logs)/2 > 1):
+                if not ( ('Probability for state |') in hdf5_logs[kk] ):
+                    hdf5_logs[kk] += (' ('+str((kk+2)//2)+' of '+str(len(hdf5_logs)//2)+')')
+            log_dict_list.append( get_dict_for_log_list(
+                log_entry_name = hdf5_logs[kk],
+                unit           = hdf5_logs[kk+1],
+                log_is_complex = save_complex_data,
+                axes = axes
+            ))
+        
+        # Save data!
+        string_arr_to_return.append(save(
+            timestamp = get_timestamp_string(),
+            ext_keys = ext_keys,
+            log_dict_list = log_dict_list,
+            
+            time_vector = time_vector,
+            fetched_data_arr = fetched_data_arr,
+            fetched_data_scale = axes['y_scaler'],
+            fetched_data_offset = axes['y_offset'],
+            resonator_freq_if_arrays_to_fft = [ readout_freq_if ],
+            
+            filepath_of_calling_script = os.path.realpath(__file__),
+            use_log_browser_database = use_log_browser_database,
+            
+            integration_window_start = integration_window_start,
+            integration_window_stop  = integration_window_stop,
+            inner_loop_size = num_freqs,
+            outer_loop_size = num_biases,
+            
+            save_complex_data = save_complex_data,
+            source_code_of_executing_file = '', #get_sourcecode(__file__),
+            default_exported_log_file_name = default_exported_log_file_name,
+            append_to_log_name_before_timestamp = '01_bias_outer' + with_or_without_bias_string,
+            append_to_log_name_after_timestamp  = '',
+            select_resonator_for_single_log_export = '',
+            
+            force_matrix_reshape_flip_row_and_column = False,
+            log_browser_tag  = log_browser_tag,
+            log_browser_user = log_browser_user,
+            save_raw_time_data = save_raw_time_data,
+        ))
+        
+    return string_arr_to_return
+
+    
