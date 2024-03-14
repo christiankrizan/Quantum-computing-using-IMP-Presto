40238c3610d61307933d26eb92a01a09c247c97f
Christian Križan <christian.krizan@gmail.com>
2024-03-14 19:13:12 +0100

    Add function for calculating two-qubit gate fidelities given decoherence times.
    The function follows (15) in https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.129.150504 - after first checking in personal conversation with the first author, I decided to calculate the pure dephasing rate and the energy relaxation rate as the average of these numbers for the two qubits. Typically, your system will never have qubits with the exact energy relaxation rates nor pure dephasing rates. Hence the average.

--- fidelity_calculator.py
+++ fidelity_calculator.py
@@ -35,3 +35,49 @@ def get_maximum_fidelity_for_x_gate(
     
     # Done!
     return fidelity_pauli_x
+
+def get_maximum_fidelity_for_2q_gate(
+        qubit1_T1,
+        qubit2_T1,
+        qubit1_T2_asterisk,
+        qubit2_T2_asterisk,
+        two_qubit_gate_time
+    ):
+    ''' Tahereh Abad told me in person that one approach to getting
+        the two-qubit gate fidelity when the two qubits are of different
+        relaxation and dephasing rates, would be to take the average of
+        the relaxation and dephasing rates.
+        
+        Using Abad et al. 2022 (15), find the maximum possible
+        fidelity for a two-qubit gate.
+        https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.129.150504
+    '''
+    # Get decoherence rates.
+    gamma_1 = 1 / qubit1_T1
+    gamma_2 = 1 / qubit2_T1
+    gamma_avg = (gamma_1 + gamma_2) / 2
+    
+    # Get the pure dephasing time, then the dephasing rate.
+    qubit1_T_phi = 1 / ((1 / qubit1_T2_asterisk) - (1 / (2*qubit1_T1)))
+    qubit2_T_phi = 1 / ((1 / qubit2_T2_asterisk) - (1 / (2*qubit2_T1)))
+    gamma1_phi = 1 / qubit1_T_phi
+    gamma2_phi = 1 / qubit2_T_phi
+    gamma_phi_avg = (gamma1_phi + gamma2_phi) / 2
+    
+    ## Split equation into more easily understandable parts
+    # Expansion part:
+    zeroth_expansion = 1
+    
+    # Multiplicand of time part:
+    N = 2     # Number of qubits.
+    d = 2**N  # Number of dimensions.
+    multiplicand = (N * d) / (2 * (d + 1))
+    
+    # Time part:
+    time_portion = two_qubit_gate_time * (gamma_avg + gamma_phi_avg)
+    
+    # Calculate fidelity!
+    fidelity_2q_gate = zeroth_expansion - (multiplicand * time_portion)
+    
+    # Done!
+    return fidelity_2q_gate
