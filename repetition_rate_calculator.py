2b5c1a43d643ac905e78b5742eb0d57e45c33bd6
Christian Križan <christian.krizan@gmail.com>
2024-10-15 16:34:08 +0200

    Add function for illustrating phase differences due to bad repetition rates.
    Quantum programs set to re-instantiate every X microseconds, will inevitably cause phase issues whenever using external mixers, if the LO frequencies of these mixers are not commensurate with the inverse of the repetition rate. This new function highlights the effect.

--- repetition_rate_calculator.py
+++ repetition_rate_calculator.py
@@ -6,6 +6,7 @@
 #############################################################################################
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def get_repetition_rate_T(
     T_begin,
@@ -73,3 +74,71 @@ def get_repetition_rate_T(
             " was set to False. The event was placed at T = "+str(T)+" s.")
         
     return T, repetition_counter
+
+def illustrate_repetition_rate(
+    calculated_repetition_rate,
+    array_of_waveform_frequencies = [],
+    array_of_waveform_frequencies_that_are_reset_on_every_repetition = []
+    ):
+    ''' Plot the calculated frequencies, illustrating where they are
+        phase commensurate.
+    '''
+    
+    # Perform user input checks.
+    if len(array_of_waveform_frequencies) <= 0:
+        raise ValueError("Could not understand the frequencies provided.")
+    if calculated_repetition_rate <= 0:
+        raise ValueError("The provided calculated repetition rate is illegal; the rate cannot be negative nor zero. The provided number was: "+str(calculated_repetition_rate))
+    
+    # For a simple initial illustration, let's find out where on the time
+    # axis that we see a few oscillations of the slowest curve. Let's say 3.
+    ## Then, we elongate the time axis to 3x that. We also put vertical lines
+    ## on 1x, 2x, and 3x the repetition rate, along the time axis.
+    # Now, let's find the slowest frequency requested. And, grab a time value.
+    lowest_frequency = np.min(array_of_waveform_frequencies_that_are_reset_on_every_repetition)
+    if lowest_frequency > np.min(array_of_waveform_frequencies):
+        lowest_frequency = np.min(array_of_waveform_frequencies)
+    period_of_lowest_frequency = 1/lowest_frequency
+    time_axis = np.linspace(0, 3*period_of_lowest_frequency, 500)
+    
+    # Stack waves in the plot!
+    stacked_waves = 0
+    for ii in range(len(array_of_waveform_frequencies)):
+        # Here, the ii portion ensures that the curve is offset.
+        y_vals = np.cos(2*np.pi * array_of_waveform_frequencies[ii] * time_axis) + ii*2 + 1.6
+        plt.plot(time_axis, y_vals, color="#034da3")
+        stacked_waves += 1
+    
+    # Coupler tones too?
+    for jj in range(len(array_of_waveform_frequencies_that_are_reset_on_every_repetition)):
+        ## Now, we must calculate this segment in parts.
+        part_y_axis = []
+        for time_item in time_axis:
+            part_y_axis.append(
+                np.cos(2*np.pi * array_of_waveform_frequencies_that_are_reset_on_every_repetition[jj] * (time_item % calculated_repetition_rate)) + (jj+stacked_waves)*2 + 1.6
+            )
+        plt.plot(time_axis, part_y_axis, color="#000000")
+    
+    # Plot repetition rate.
+    done = False
+    maximum_iterator = 0
+    while (not done) and (maximum_iterator <= 10):
+        if (calculated_repetition_rate * maximum_iterator) < time_axis[-1]:
+            plt.axvline(x=(calculated_repetition_rate * maximum_iterator), color="#ef1620", linestyle='--')
+        else:
+            done = True
+        maximum_iterator += 1
+        if maximum_iterator == 10:
+            raise ValueError("Halted! You are plotting too many repetition rate lines.")
+    
+    # Prepare the plot.
+    plt.title('Phase error from bad repetition rates', fontsize=22)
+    plt.xlabel('Time [s]', fontsize=22)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    
+    # Remove the Y axis.
+    plt.yticks([])
+    
+    # Show the plot!
+    plt.show()
