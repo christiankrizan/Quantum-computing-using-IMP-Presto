07446c7bc690b0b9d6fcdf4fb76422de02a66411
Christian Križan <christian.krizan@gmail.com>
2025-03-19 10:57:24 +0100

    Add function for calculating NCO frequency groups given a list of known qubits.
    This function also works for resonator frequencies and finding a suitable NCO frequency for those.

--- nco_calculator.py
+++ nco_calculator.py
@@ -5,6 +5,7 @@
 #   https://github.com/christiankrizan/Quantum-computing-using-IMP-Presto/blob/master/LICENSE
 #############################################################################################
 
+import itertools
 import numpy as np
 
 def get_safe_dac_nco_freq(
@@ -52,5 +53,54 @@ def get_safe_dac_nco_freq(
     
     # Return.
     return final_nco_freq
+
+def calculate_nco_groups_from_frequency_lists(
+    frequency_list,
+    baseband_bandwidth_complex,
+    nco_guard_band_complex,
+    number_of_frequency_groups = 1,
+    ):
+    ''' Given a list of frequencies, calculate NCO frequencies for
+        n groups, that can still target all qubits.
+        
+        baseband_bandwidth_complex: The available bandwidth.
+        Note: 1000e6 = 1000e6 = from -500e6 to +500e6 Hz.
+        
+        nco_guard_band_complex: The bandwidth around the NCO signal itself
+        that no signal may enter. If set to 20 MHz, for instance, then
+        no signal may be sent within ±10 MHz of the NCO itself.
+    '''
     
+    ## TODO!
+    if number_of_frequency_groups != 2:
+        raise NotImplementedError("Halted! Only 2 group are supported as of yet. TODO!")
+    
+    # Function to check if a given NCO fits a set of frequencies
+    def is_valid_nco(nco, group):
+        return all(nco_guard_band_complex/2 <= abs(f - nco) <= baseband_bandwidth_complex/2 for f in group)
+
+    # Try all ways to split into two groups of four signals each
+    for group1 in itertools.combinations(frequency_list, int(np.ceil(len(frequency_list)/number_of_frequency_groups))):
+        
+        ## TODO! More groups pls.
+        group2 = tuple(set(frequency_list) - set(group1))
+        
+        # Generate potential NCOs as the midpoint of each group range
+        for nco1 in np.linspace(min(group1) + 0.01, max(group1) - 0.01, 100):
+            if is_valid_nco(nco1, group1):
+                for nco2 in np.linspace(min(group2) + 0.01, max(group2) - 0.01, 100):
+                    if is_valid_nco(nco2, group2):
+                        ## TODO! More groups pls.
+                        solution = {
+                            "Group 1": group1, "NCO 1": nco1,
+                            "Group 2": group2, "NCO 2": nco2
+                        }
+                        break
+                if 'solution' in locals():
+                    break
+        if 'solution' in locals():
+            break
+    
+    # Return the solution found!
+    return solution
     
